a6c8611600efd88649ce7358105ca605f8e5d10f
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2024-08-16 16:13:10 -0500

    style: format the code of the tab layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -8,8 +8,18 @@ const TabIcon = ({ icon, color, name, focused }) => {
   return (
     <>
       <View className="items-center justify-center gap-2">
-        <Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" />
-        <Text className={`${focused ? 'font-semibold' : 'font-normal'} text-xs`} style={{color: color}}>{name}</Text>
+        <Image
+          source={icon}
+          resizeMode="contain"
+          tintColor={color}
+          className="w-6 h-6"
+        />
+        <Text
+          className={`${focused ? "font-semibold" : "font-normal"} text-xs`}
+          style={{ color: color }}
+        >
+          {name}
+        </Text>
       </View>
     </>
   );
@@ -20,14 +30,14 @@ export default function TabsLayout() {
     <Tabs
       screenOptions={{
         tabBarShowLabel: false,
-        tabBarActiveTintColor: '#47B370',
-        tabBarInactiveTintColor: '#E8E5E0',
+        tabBarActiveTintColor: "#47B370",
+        tabBarInactiveTintColor: "#E8E5E0",
         tabBarStyle: {
-          backgroundColor: '#1D2D4C',
+          backgroundColor: "#1D2D4C",
           borderTopWidth: 1,
-          borderTopColor: '#1D2D4C',
-          height: 84
-        }
+          borderTopColor: "#1D2D4C",
+          height: 84,
+        },
       }}
     >
       <Tabs.Screen
