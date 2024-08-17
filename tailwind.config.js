76cf166db9cf7d64a7916ab2082ac9bc2be0b79b
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2024-08-17 13:18:49 -0500

    feat: configure fonts and colors with NativeWind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D2D4C",
+      },
+    },
+    fontFamily: {
+      nextralight: ["Nunito-ExtraLight", "sans-serif"],
+      nextralightitalic: ["Nunito-ExtraLightItalic", "sans-serif"],
+      nlight: ["Nunito-Light", "sans-serif"],
+      nlightitalic: ["Nunito-LightItalic", "sans-serif"],
+      nregular: ["Nunito-Regular", "sans-serif"],
+      nitalic: ["Nunito-Italic", "sans-serif"],
+      nmedium: ["Nunito-Medium", "sans-serif"],
+      nmediumitalic: ["Nunito-MediumItalic", "sans-serif"],
+      nsemibold: ["Nunito-SemiBold", "sans-serif"],
+      nsemibolditalic: ["Nunito-SemiBoldItalic", "sans-serif"],
+      nbold: ["Nunito-Bold", "sans-serif"],
+      nbolditalic: ["Nunito-BoldItalic", "sans-serif"],
+      nextrabold: ["Nunito-ExtraBold", "sans-serif"],
+      nextrabolditalic: ["Nunito-ExtraBoldItalic", "sans-serif"],
+      nblack: ["Nunito-Black", "sans-serif"],
+      nblackitalic: ["Nunito-BlackItalic", "sans-serif"],
+    },
   },
   plugins: [],
 };
