4dc63a8fa5194959ebb7c03df63d584656b696ef
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2024-08-21 22:10:26 -0500

    fix: correct fonts not found

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,49 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Nunito-ExtraLight": require("../assets/fonts/Nunito-ExtraLight.ttf"),
+    "Nunito-ExtraLightItalic": require("../assets/fonts/Nunito-ExtraLightItalic.ttf"),
+    "Nunito-Light": require("../assets/fonts/Nunito-Light.ttf"),
+    "Nunito-LightItalic": require("../assets/fonts/Nunito-LightItalic.ttf"),
+    "Nunito-Regular": require("../assets/fonts/Nunito-Regular.ttf"),
+    "Nunito-Italic": require("../assets/fonts/Nunito-Italic.ttf"),
+    "Nunito-Medium": require("../assets/fonts/Nunito-Medium.ttf"),
+    "Nunito-MediumItalic": require("../assets/fonts/Nunito-MediumItalic.ttf"),
+    "Nunito-SemiBold": require("../assets/fonts/Nunito-SemiBold.ttf"),
+    "Nunito-SemiBoldItalic": require("../assets/fonts/Nunito-SemiBoldItalic.ttf"),
+    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-BoldItalic": require("../assets/fonts/Nunito-BoldItalic.ttf"),
+    "Nunito-ExtraBold": require("../assets/fonts/Nunito-ExtraBold.ttf"),
+    "Nunito-ExtraBoldItalic": require("../assets/fonts/Nunito-ExtraBoldItalic.ttf"),
+    "Nunito-Black": require("../assets/fonts/Nunito-Black.ttf"),
+    "Nunito-BlackItalic": require("../assets/fonts/Nunito-BlackItalic.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
   );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -8,29 +8,10 @@ import CustomButton from "../components/CustomButton";
 import { StatusBar } from "expo-status-bar";
 
 const App = () => {
-  const [fontsLoaded, error] = useFonts({
-    "Nunito-ExtraLight": require("../assets/fonts/Nunito-ExtraLight.ttf"),
-    "Nunito-ExtraLightItalic": require("../assets/fonts/Nunito-ExtraLightItalic.ttf"),
-    "Nunito-Light": require("../assets/fonts/Nunito-Light.ttf"),
-    "Nunito-LightItalic": require("../assets/fonts/Nunito-LightItalic.ttf"),
-    "Nunito-Regular": require("../assets/fonts/Nunito-Regular.ttf"),
-    "Nunito-Italic": require("../assets/fonts/Nunito-Italic.ttf"),
-    "Nunito-Medium": require("../assets/fonts/Nunito-Medium.ttf"),
-    "Nunito-MediumItalic": require("../assets/fonts/Nunito-MediumItalic.ttf"),
-    "Nunito-SemiBold": require("../assets/fonts/Nunito-SemiBold.ttf"),
-    "Nunito-SemiBoldItalic": require("../assets/fonts/Nunito-SemiBoldItalic.ttf"),
-    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
-    "Nunito-BoldItalic": require("../assets/fonts/Nunito-BoldItalic.ttf"),
-    "Nunito-ExtraBold": require("../assets/fonts/Nunito-ExtraBold.ttf"),
-    "Nunito-ExtraBoldItalic": require("../assets/fonts/Nunito-ExtraBoldItalic.ttf"),
-    "Nunito-Black": require("../assets/fonts/Nunito-Black.ttf"),
-    "Nunito-BlackItalic": require("../assets/fonts/Nunito-BlackItalic.ttf"),
-  });
-
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+        <View className="w-full justify-center items-center min-h-[80vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
