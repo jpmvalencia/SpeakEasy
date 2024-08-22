c07bf02d7a19d97e207984c11ab17f8169637961
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2024-08-21 22:11:17 -0500

    feat: add sign in and sign up screens

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,81 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const submit = () => {};
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[80vh] px-4 my-6">
+          <View className="items-center">
+            <Image
+              source={images.logo}
+              className="w-[78px] h-[35px]"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-2xl text-white font-nsemibold mt-10">
+            Registrate
+          </Text>
+
+          <FormField
+            title="Usuario"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-7"
+          />
+
+          <FormField
+            title="Correo Electrónico"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Contraseña"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Regístrate"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-nregular">
+              ¿Tienes una cuenta?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-nsemibold text-secondary"
+            >
+              Inicia Sesión
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
